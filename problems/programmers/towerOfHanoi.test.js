b025a84fb0ac63cd5d7285e70b864041ee794435
Shubidumdu <skaty4870@naver.com>
2021-01-18 21:10:06 +0900

    Solve 하노이의 탑

--- problems/programmers/towerOfHanoi.test.js
+++ problems/programmers/towerOfHanoi.test.js
@@ -1,71 +1,48 @@
 import doTests from '../../do.test';
 
-const inputs = [[2]];
+const inputs = [[2], [3]];
 const results = [
   [
     [1, 2],
     [1, 3],
     [2, 3],
   ],
+  [
+    [1, 3],
+    [1, 2],
+    [3, 2],
+    [1, 3],
+    [2, 1],
+    [2, 3],
+    [1, 3],
+  ],
 ];
 
-const makeHanoi = (n) => {
-  const result = [...Array(3)].map(() => []);
-  result[0] = [...Array(n).keys()].map((n) => n + 1).reverse();
-  return result;
-};
-
-const getTops = (hanoi) => {
-  const tops = hanoi.map((arr) => arr[arr.length - 1] || 0);
-  return tops;
-};
-
-const moveBlock = (result, hanoi, from, to) => {
-  const block = hanoi[from].pop();
-  hanoi[to].push(block);
-  result.push([from, to]);
-  return hanoi;
+const hanoi = (n) => {
+  if (n === 1) return [[1, 3]];
+  const hanoi_before = hanoi(n - 1);
+  const before = hanoi_before.map((numbers) => {
+    return numbers.map((n) => {
+      if (n === 3) return 2;
+      if (n === 2) return 3;
+      return n;
+    });
+  });
+  const after = hanoi_before.map((numbers) => {
+    return numbers.map((n) => {
+      if (n === 1) return 2;
+      if (n === 2) return 1;
+      return n;
+    });
+  });
+  return [...before, [1, 3], ...after];
 };
 
 const solution = (n) => {
-  const hanoi = makeHanoi(n);
-  let target = n;
-  let targetIdx = 0;
-  const result = [];
-  while (true) {
-    if (hanoi[2].includes(target)) target -= 1;
-    if (target < 1) break;
-    hanoi.forEach((arr, idx) => {
-      if (arr.includes(target)) targetIdx = idx;
-    });
-    const tops = getTops(hanoi);
-    const [left, mid, right] = tops;
-    const minIdx = tops.indexOf(Math.min(...tops));
-    const maxIdx = tops.indexOf(Math.max(...tops));
-    const midIdx = tops.find((val, idx) => idx !== minIdx && idx !== maxIdx);
-    // 최초에 아무것도 옮겨지지 않았을 때
-    if (!mid && !right) {
-      moveBlock(result, hanoi, 0, (n % 2) + 1);
-      continue;
-    }
-    // // 상단에 목표가 보이는 경우
-    // if (tops.indexOf(target) !== -1) {
-    //   // 끝으로 옮길 수 있으면 옮긴다.
-    //   if (right === 0 || right > target) {
-    //     moveBlock(result, hanoi, targetIdx, 2);
-    //     continue;
-    //   }
-    // }
-    // 빈 공간이 있는 경우
-    if (!mid || !right) {
-      const maxIdx = tops.indexOf(Math.max(...tops));
-      const emptyIdx = tops.indexOf(0);
-      moveBlock(result, hanoi, maxIdx, emptyIdx);
-      continue;
-    }
-  }
+  const answer = hanoi(n);
+  console.log(answer);
 
-  return;
+  return answer;
 };
 
 doTests(inputs, results, solution);
