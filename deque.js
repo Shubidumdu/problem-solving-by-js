672629670f9380aea047ba26cea423845f433ae4
Shubidumdu <skaty4870@naver.com>
2021-05-08 02:08:58 +0900

    Make deque

---
+++ deque.js
@@ -0,0 +1,35 @@
+class Deque {
+  _first = 0;
+  _last = -1;
+  _map = null;
+
+  constructor(arr) {
+    this._map = new Map();
+    arr &&
+      arr.forEach((val, idx) => {
+        this._last++;
+        this._map.set(idx, val);
+      });
+  }
+
+  push(val) {
+    this._last++;
+    this._map.set(this._last, val);
+  }
+
+  pop() {
+    if (!this._map.size) return undefined;
+    const lastValue = this._map.get(this._last);
+    this._map.delete(this._last);
+    this._last--;
+    return lastValue;
+  }
+
+  shift() {
+    if (!this._map.size) return undefined;
+    const firstValue = this._map.get(this._first);
+    this._map.delete(this._first);
+    this._first++;
+    return firstValue;
+  }
+}
